f158e840253d8f1e59a1d6823b5183d8dc7ebaef
Ian Kelly <idk13@hotmail.com>
2023-02-23 16:48:03 +0100

    Green lit the rspec

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,41 @@
+class BookmarksController < ApplicationController
+
+  def new
+    set_list
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    set_list
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  # def list_params
+  #   params.require(:id).permit(:content)
+  # end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  # get 'bookmarks/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get 'lists',      to: 'lists#index'
+  # get 'lists/new',  to: 'lists#new'
+  # post 'lists',     to: 'lists#create'
+  # get 'lists/:id',  to: 'lists#show'
+
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
+
 end
